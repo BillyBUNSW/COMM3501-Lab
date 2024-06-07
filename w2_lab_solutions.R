d0cfae3c0c24f82f1a01e20e93346a2f5315293f
BillyBUNSW <124408819+BillyBUNSW@users.noreply.github.com>
2024-06-07 11:58:54 +1000

    Update w2_lab_solutions.R

--- w2_lab_solutions.R
+++ w2_lab_solutions.R
@@ -1,7 +1,9 @@
-# Question 25
+# Question 25 - Regression Outcomes
 
 library(readxl) 
 TV_data <- read_excel("jaggia_ba_2e_ch08_data.xlsx", sheet="Television") 
+
+# Fitting a quadratic model is the same as a linear model, but with different predictors
 TV_fit <- lm(GPA ~ Hours + I(Hours^2), data=TV_data) 
 summary(TV_fit) 
 plot(TV_data$Hours, TV_data$GPA)
@@ -13,10 +15,13 @@ plot(TV_data$Hours, TV_data$GPA)
 #per week. Therefore, this study suggests that 9.36 is the optimal number of hours of TV for 
 #middle school students to maximize their GPA. 
 
-# Question 41
+# Question 41 - Regression Outcomes
 
 # Hold-out method
 IceCream <-  read_excel("jaggia_ba_2e_ch08_data.xlsx", sheet="IceCream") 
+# How to subset dataframe: df[row_indices, col_indices]. 
+# Use vector shortcuts such as a:b to create a vector of integers to subset.
+# Indexing starts from 1 in R
 IceCream_Train <- IceCream[1:24,]
 IceCream_Valid <- IceCream[25:35,] 
 
@@ -24,6 +29,7 @@ IceCream_Valid <- IceCream[25:35,]
 
 Model1 <- lm(Income ~ Hours + Hot + Holiday, data = IceCream_Train) 
 summary(Model1) 
+#Making predictions on the validation set
 Pred1 <- predict(Model1, IceCream_Valid) 
 RMSE1 = sqrt(mean((IceCream_Valid$Income-Pred1)^2)) 
 
@@ -34,23 +40,12 @@ summary(Model2)
 Pred2 <- predict(Model2, IceCream_Valid) 
 RMSE2 = sqrt(mean((IceCream_Valid$Income-Pred2)^2)) 
 
-# Compare RMSEs, model 2 has higher predictability
+# Compare RMSEs, model 2 has higher predictability because it has lower RMSE
 RMSE1; RMSE2
 
-#Cross-validation
-library(caret) 
-train.control <- trainControl(method = "cv", number=5) 
-model3 <- train(Income ~ Hours + Hot + Holiday,  
-                data = TData, method = "lm",  trControl = train.control) 
-summary(model3) 
-print(model3) 
-model4 <- train(Income ~ Hours + Hot + Holiday + Hot*Holiday, 
-                data = TData, method = "lm", 
-                trControl = train.control) 
-summary(model4) 
 
 
-# Question 43
+# Question 43 - Logistic Regression
 library(readxl) 
 library(caret) 
 Default <- read_excel("jaggia_ba_2e_ch09_data.xlsx", sheet="Default") 
@@ -59,7 +54,12 @@ Default_Train <- Default[1:300,]
 Default_Valid <- Default[301:400,] 
 
 logi_model <- glm(Default ~ LTV + FICO + Age, family = binomial, data = Default_Train) 
+summary(logi_model)
+# notice how the z-score has replaced the t-score.
+# this is because GLMs are estimated through MLE whose parameters are normal
+
 default_probs <- predict(logi_model, Default_Valid, type = "response")
+# predicts probabilities of default on the validation dataset
 
 # Creating predictions based on natural decision boundary 50%
 default_pred1 <- ifelse(default_probs >= 0.5, 1,0) 
@@ -73,4 +73,4 @@ default_pred2 <- ifelse(default_probs >= mean(Default_Train$Default), 1,0)
 result2 <- data.frame(predicted=as.factor(default_pred2), default=as.factor(Default_Valid$Default)) 
 confusionMatrix(result2$predicted,result2$default, positive="1")  
 
-# It might be better to use the proportion as decision boundary because the sensitivity increases significantlu
+# It might be better to use the proportion as decision boundary because the sensitivity increases significantly
